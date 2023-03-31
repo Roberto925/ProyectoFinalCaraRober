331a8a052bc1d110ea8790e3c644d5233899b891
Roberto925 <118230753+Roberto925@users.noreply.github.com>
2023-03-31 20:38:21 -0300

    Delete myapp/myapp directory

--- myapp/myapp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-admin.site.register(Paciente)
-admin.site.register(Doctor)
-admin.site.register(Turno)
-admin.site.register(Avatar)
-# Register your models here.
